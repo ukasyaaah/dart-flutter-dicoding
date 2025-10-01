dd6187b1edaa9b42c18c2edbe8dd21aad2ff56ac
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-10-01 22:26:01 +0700

    add: image preview

--- wisatakota/lib/main.dart
+++ wisatakota/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wisata Kota',
       theme: ThemeData(),
       home: DetailScreen(),
@@ -24,14 +25,15 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Image.asset('assets/images/farm-house.jpg'),
             Container(
               margin: EdgeInsets.only(top: 16),
               child: Text(
-                'Lawang Sewu Semarang',
+                'Farm House Lembang',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
@@ -83,6 +85,41 @@ class DetailScreen extends StatelessWidget {
               child: Text(
                 textAlign: TextAlign.center,
                 'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: BorderRadiusGeometry.circular(12),
+                      child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: BorderRadiusGeometry.circular(12),
+                      child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: BorderRadiusGeometry.circular(12),
+                      child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg',
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
